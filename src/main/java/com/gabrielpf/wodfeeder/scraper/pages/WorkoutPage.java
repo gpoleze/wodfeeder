3df7e2bdffb79aad6a8da9b8ac78844353fc5818
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-01-23 23:41:50 -0800

    Implemented the WorkoutPage and added its tests

---
+++ src/main/java/com/gabrielpf/wodfeeder/scraper/pages/WorkoutPage.java
@@ -0,0 +1,20 @@
+package com.gabrielpf.wodfeeder.scraper.pages;
+
+import org.jsoup.nodes.Document;
+
+import java.net.UnknownHostException;
+
+public class WorkoutPage extends BasePage {
+
+	public WorkoutPage(String url) throws UnknownHostException {
+		super(url);
+	}
+
+	public String getLastPostLink(String cssSelection) throws NoSuchFieldException {
+
+		return document
+				.select(cssSelection)
+				.get(0)
+				.attr("href");
+	}
+}
