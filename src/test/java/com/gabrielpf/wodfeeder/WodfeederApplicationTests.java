5d7ec7eaf4a07e85ae1d647c90fd68e319da9de9
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-02-03 15:51:37 -0800

    added .DS_SORE to gitignore

---
+++ src/test/java/com/gabrielpf/wodfeeder/WodfeederApplicationTests.java
@@ -0,0 +1,18 @@
+package com.gabrielpf.wodfeeder;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+//@RunWith(JUnitPlatform.class)
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class WodfeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
