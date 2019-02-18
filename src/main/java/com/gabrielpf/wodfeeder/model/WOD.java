68cb0d1989aa192d3848a66fe6fcd86cae76113f
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-02-17 19:28:17 -0800

    Added the persistence dependenices and implemented the WOD model

--- src/main/java/com/gabrielpf/wodfeeder/model/WOD.java
+++ src/main/java/com/gabrielpf/wodfeeder/model/WOD.java
@@ -1,13 +1,47 @@
 package com.gabrielpf.wodfeeder.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(schema = "public", name = "workoutOfTheDay")
 public class WOD {
-	private LocalDate day;
-	private String exercises;
 
-	public WOD(LocalDate day, String exercises) {
-		this.day = day;
-		this.exercises = exercises;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date", nullable = false, unique = true)
+    private LocalDate date;
+
+    @Column(name = "exercises", nullable = false)
+    private String exercises;
+
+    public WOD(LocalDate date, String exercises) {
+        this.date = date;
+        this.exercises = exercises;
+    }
+
+    private WOD() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getExercises() {
+        return exercises;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setExercises(String exercises) {
+        this.exercises = exercises;
+    }
 }
