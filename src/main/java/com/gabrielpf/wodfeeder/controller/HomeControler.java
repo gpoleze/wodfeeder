c6f89a3da33b1369ec59335741c1dbad59c75b6d
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-02-24 22:38:31 -0800

    Add method to re-load the base we page scrapping it again

--- src/main/java/com/gabrielpf/wodfeeder/controller/HomeControler.java
+++ src/main/java/com/gabrielpf/wodfeeder/controller/HomeControler.java
@@ -1,10 +1,13 @@
 package com.gabrielpf.wodfeeder.controller;
 
+import com.gabrielpf.wodfeeder.ScheduledTasks;
 import com.gabrielpf.wodfeeder.vo.WodVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.time.LocalDate;
 import java.util.Map;
@@ -16,6 +19,9 @@ public class HomeControler {
     @Autowired
     private WodVO vo;
 
+    @Autowired
+    private ScheduledTasks tasks;
+
     //    @RequestMapping(value = "/", produces = "application/json")
     @RequestMapping(value = "/")
     public String home(Model model) {
@@ -25,5 +31,14 @@ public class HomeControler {
         model.addAttribute("latestWorkouts", new TreeMap<>(localDateStringMap));
 
         return "home";
+    }
+
+    @RequestMapping("/reload")
+    public RedirectView reloadWorkouts(Model model, RedirectAttributes attributes) {
+        tasks.scrapThisWeekWorkout();
+
+        attributes.addFlashAttribute("flashAttribute", "reload");
+
+        return new RedirectView("/");
     }
 }
