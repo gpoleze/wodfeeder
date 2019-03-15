b150a128f2f530f3be9d2c58e469f8b5cebe37e4
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-03-14 18:34:59 -0700

    re-indented to match the project standard

--- src/main/java/com/gabrielpf/wodfeeder/scraper/pages/utils/DailyWorkoutReader.java
+++ src/main/java/com/gabrielpf/wodfeeder/scraper/pages/utils/DailyWorkoutReader.java
@@ -12,50 +12,51 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DailyWorkoutReader {
-	private final Elements paragraphs;
-	private final Map<DayOfWeek, Integer> daylyWorkouts;
+    private final Elements paragraphs;
+    private final Map<DayOfWeek, Integer> daylyWorkouts;
 
-	public DailyWorkoutReader(Elements paragraphs) {
-		this.paragraphs = paragraphs;
-		daylyWorkouts = generateListOfWorkouts();
-	}
+    public DailyWorkoutReader(Elements paragraphs) {
+        this.paragraphs = paragraphs;
+        daylyWorkouts = generateListOfWorkouts();
+    }
 
-	/**
-	 * Reads the paragraphs of a html to find the index where the days of the week are.
-	 *
-	 * @return a map for each day of the week its index position
-	 */
-	private Map<DayOfWeek, Integer> generateListOfWorkouts() {
+    /**
+     * Reads the paragraphs of a html to find the index where the days of the week are.
+     *
+     * @return a map for each day of the week its index position
+     */
+    private Map<DayOfWeek, Integer> generateListOfWorkouts() {
 
-		HashMap<DayOfWeek, Integer> workoutsStartLine = new HashMap<>();
+        HashMap<DayOfWeek, Integer> workoutsStartLine = new HashMap<>();
 
-		Arrays.stream(DayOfWeek.values()).forEach(day -> {
-			OptionalInt line = IntStream.range(0, paragraphs.size())
-					.filter(i -> paragraphs.get(i).text().toUpperCase().equals(day.name()))
-					.findFirst();
-			workoutsStartLine.put(day, line.getAsInt());
-		});
-		return Collections.unmodifiableMap(workoutsStartLine);
-	}
+        Arrays.stream(DayOfWeek.values()).forEach(day -> {
+            OptionalInt line = IntStream.range(0, paragraphs.size())
+                    .filter(i -> paragraphs.get(i).text().toUpperCase().equals(day.name()))
+                    .findFirst();
+            workoutsStartLine.put(day, line.getAsInt());
+        });
+        return Collections.unmodifiableMap(workoutsStartLine);
+    }
 
-	/**
-	 * For a given day of the week, return its workouts
-	 * @param day the day of week to get the workout
-	 * @return the workout for that day of week
-	 */
-	public String getWorkoutForDay(DayOfWeek day) {
-		if (day == DayOfWeek.SUNDAY)
-			return "Rest";
+    /**
+     * For a given day of the week, return its workouts
+     *
+     * @param day the day of week to get the workout
+     * @return the workout for that day of week
+     */
+    public String getWorkoutForDay(DayOfWeek day) {
+        if (day == DayOfWeek.SUNDAY)
+            return "Rest";
 
-		DayOfWeek nextDay = day.plus(1);
+        DayOfWeek nextDay = day.plus(1);
 
-		String s = IntStream.range(0, paragraphs.size())
-				.filter(i -> i > daylyWorkouts.get(day) && i < daylyWorkouts.get(nextDay))
-				.boxed()
-				.map(i -> paragraphs.get(i).text())
-				.collect(Collectors.joining("\n"));
+        String s = IntStream.range(0, paragraphs.size())
+                .filter(i -> i > daylyWorkouts.get(day) && i < daylyWorkouts.get(nextDay))
+                .boxed()
+                .map(i -> paragraphs.get(i).text())
+                .collect(Collectors.joining("\n"));
 
-		return s;
-	}
+        return s;
+    }
 
 }
