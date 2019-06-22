835d5731a636656f1dfe12be72e3a0e439401be8
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-06-21 22:49:32 -0700

    Added routing to the Login form

--- src/client/wodfeeder/src/index.js
+++ src/client/wodfeeder/src/index.js
@@ -9,16 +9,17 @@ import {store} from "./store/store";
 
 import './index.css';
 import {reloadWorkouts} from "./store/workouts/workouts.thunk";
-
+import Login from "./containers/login/login.page";
 
 ReactDOM.render(
     <Provider store={store}>
-        <Header/>
         <BrowserRouter>
+            <Header/>
             <Switch>
                 <Route exact path='/' component={App}/>
                 <Route path='/week' component={App}/>
                 <Route path='/reload' component={reloadWorkouts}/>
+                <Route path='/login' component={Login}/>
                 <Route component={() => <div>'Page not Found 404'</div>}/>
             </Switch>
         </BrowserRouter>
