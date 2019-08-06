04e4fafc04e7ed7a8c804a74320c6938e04edc4b
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-08-06 07:44:57 -0700

    fixed typo

--- src/client/wodfeeder/src/store/app-bar/sign-in/login.initial-state.js
+++ src/client/wodfeeder/src/store/app-bar/sign-in/login.initial-state.js
@@ -1,6 +1,6 @@
 import {InputVO} from "../../../components/forms/InputVO";
 
-export const loginForminitialState = {
+export const loginFormInitialState = {
     loginForm: {
         username: new InputVO('username', 'Username', 'username', value => console.log(value)),
         password: new InputVO('password', 'Password', 'password', value => console.log(value), 'password'),
--- src/client/wodfeeder/src/store/initial-state.js
+++ src/client/wodfeeder/src/store/initial-state.js
@@ -1,10 +1,10 @@
 import {workoutsInitialState} from "./workouts/workouts.initial-state";
-import {loginForminitialState} from "./app-bar/sign-in/login.initial-state";
+import {loginFormInitialState} from "./app-bar/sign-in/login.initial-state";
 
 export const initialState = {
     ...workoutsInitialState,
     forms:{
         ...workoutsInitialState.forms,
-        ...loginForminitialState
+        ...loginFormInitialState
     }
 };
