b7605a12c6c55794c914904d15ba79d44a3f97fe
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2020-07-04 17:46:57 -0700

    initial commit

---
+++ src/main/java/com/gabrielpf/wodfeeder/WodfeederApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielpf.wodfeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodfeederApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WodfeederApplication.class, args);
+    }
+
+}
