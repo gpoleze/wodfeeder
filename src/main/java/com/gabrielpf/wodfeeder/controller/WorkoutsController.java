8f1cee3b693a8cea44953b76112665aee4247baf
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2020-07-13 22:52:31 -0700

    Added GET /workouts/{date} endpoint

--- src/main/java/com/gabrielpf/wodfeeder/controller/WorkoutsController.java
+++ src/main/java/com/gabrielpf/wodfeeder/controller/WorkoutsController.java
@@ -1,11 +1,14 @@
 package com.gabrielpf.wodfeeder.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,10 @@ public class WorkoutsController {
     @GetMapping
     public List<WorkoutVO> workouts(@PageableDefault(size=50) Pageable pageable){
         return workoutService.findAll(pageable);
+    }
+
+    @GetMapping("/{date}")
+    public List<WorkoutVO> workouts(@PathVariable LocalDate date){
+        return workoutService.findAllByDate(date);
     }
 }
