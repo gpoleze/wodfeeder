6e93b0ad1ce68d70dfcc97692d825432c8ea33ec
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2020-07-23 17:49:32 -0700

    Removed the controller generated automatically by the Spring Rest

--- src/main/java/com/gabrielpf/wodfeeder/repo/WorkoutRepo.java
+++ src/main/java/com/gabrielpf/wodfeeder/repo/WorkoutRepo.java
@@ -5,12 +5,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.gabrielpf.wodfeeder.model.Workout;
 
 @Repository
-public interface WorkoutRepo extends PagingAndSortingRepository<Workout, UUID> {
+@RestResource(exported = false)
+public interface WorkoutRepo extends JpaRepository<Workout, UUID> {
     Optional<List<Workout>> findByDate(LocalDate date);
 }
