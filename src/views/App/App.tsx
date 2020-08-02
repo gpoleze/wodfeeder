bb6f0b0b4cf64c1a29254830151e79d37f4ff2cc
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2020-08-02 11:58:38 -0700

    Adjusted existing files to be according to esling/prettier patterns

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
+import Table from "@material-ui/core/Table";
+
+import Title from "components/Title";
+
+const App: React.FC = () => (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Title>WoodFeeder</Title>
+        <Table />
+        <header className="App-header">
+            <p>
+                Edit
+                <code>src/App.tsx</code>
+                and save to reload.
+            </p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                Learn React
+            </a>
+        </header>
     </div>
-  );
-}
+);
 
 export default App;
