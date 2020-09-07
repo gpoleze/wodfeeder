5157ed9d9df26c29f7bc64be6b7a99220f4822e1
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2020-09-07 00:22:44 -0700

    Adding cat to read the dotEnv

--- .github/custom-scripts/web-client-version.sh
+++ .github/custom-scripts/web-client-version.sh
@@ -4,6 +4,8 @@ sha="$(git rev-parse --short HEAD)"
 timestamp="$(date --utc +%FT%TZ)"
 dotEnv="$(pwd)/../../web-client/.env"
 
-appVersion="${timestamp}-${sha}"
+appVersion="$timestamp-$sha"
 
-sed -i -e "s/%WEB_CLIENT_VERSION%/${sha}/g" $dotEnv
+sed -i -e "s/%WEB_CLIENT_VERSION%/$appVersion/g" "$dotEnv"
+
+cat "$dotEnv"
