1c89548473dcaac7d7b678456d71c48567aeafe1
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2020-09-07 22:59:22 -0700

    Modified web-client-version.sh script

--- .github/workflows/web-client/web-client-version.sh
+++ .github/workflows/web-client/web-client-version.sh
@@ -2,10 +2,12 @@
 
 sha="$(git rev-parse --short HEAD)"
 timestamp="$(date --utc +%FT%TZ)"
-dotEnv="$(pwd | awk -F '.github' '{print $1}')web-client/.env"
 
 appVersion="$timestamp-$sha"
+echo "$appVersion"
 
-sed -i -e "s/%WEB_CLIENT_VERSION%/$appVersion/g" "$dotEnv"
-
-cat "$dotEnv"
+curl -n -X PATCH "https://api.heroku.com/apps/wf-web-client/config-vars" \
+  -H "Authorization: Bearer ${{ secrets.HEROKU_API_TOKEN }}" \
+  -H "Accept: application/vnd.heroku+json; version=3" \
+  -H "Content-Type: application/json" \
+  -d "{\"REACT_APP_WEB_CLIENT_VERSION\": \"$appVersion\"}"
