6eae80e5834ea6b542ef8114e0d547aaa214c782
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2020-09-07 23:13:11 -0700

    I miss GitLab so much...

--- .github/workflows/web-client/web-client-version.sh
+++ .github/workflows/web-client/web-client-version.sh
@@ -7,7 +7,7 @@ appVersion="$timestamp-$sha"
 echo "$appVersion"
 
 curl -n -X PATCH "https://api.heroku.com/apps/wf-web-client/config-vars" \
-  -H "Authorization: Bearer $TOKEN" \
+  -H "Authorization: Bearer ${secrets.HEROKU_API_TOKEN}" \
   -H "Accept: application/vnd.heroku+json; version=3" \
   -H "Content-Type: application/json" \
   -d "{\"REACT_APP_WEB_CLIENT_VERSION\": \"$appVersion\"}"
